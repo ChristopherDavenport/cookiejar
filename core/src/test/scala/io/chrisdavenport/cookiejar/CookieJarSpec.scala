e4fd76a87406a00581f96edbb020df5795f0da20
Chris Davenport <chris@christopherdavenport.tech>
2019-08-13 13:22:23 -0700

    Changes

--- core/src/test/scala/io/chrisdavenport/cookiejar/CookieJarSpec.scala
+++ core/src/test/scala/io/chrisdavenport/cookiejar/CookieJarSpec.scala
@@ -26,86 +26,86 @@ class CookieJarSpec extends Specification{
     }
   }
 
-  "isExpiredByExpiration" should {
-    "show an expiration in the past as expired" in {
-      val now = HttpDate.unsafeFromEpochSecond(5000)
-      val expired = HttpDate.unsafeFromEpochSecond(0)
-      val key = CookieKey("foo", None, None)
-      val value = CookieValue(expired, ResponseCookie(
-        "foo",
-        "bar",
-        expires = Some(expired)
-      ))
-      CookieJar.isExpiredByExpiration(now)((key, value)) must_=== true
-    }
+  // "isExpiredByExpiration" should {
+  //   "show an expiration in the past as expired" in {
+  //     val now = HttpDate.unsafeFromEpochSecond(5000)
+  //     val expired = HttpDate.unsafeFromEpochSecond(0)
+  //     val key = CookieKey("foo", None, None)
+  //     val value = CookieValue(expired, ResponseCookie(
+  //       "foo",
+  //       "bar",
+  //       expires = Some(expired)
+  //     ))
+  //     CookieJar.isExpiredByExpiration(now)((key, value)) must_=== true
+  //   }
 
-    "show a cookie that is still valid" in {
-      val now = HttpDate.unsafeFromEpochSecond(0) 
-      val expired = HttpDate.unsafeFromEpochSecond(5000)
-      val key = CookieKey("foo", None, None)
-      val value = CookieValue(expired, ResponseCookie(
-        "foo",
-        "bar",
-        expires = Some(expired)
-      ))
-      CookieJar.isExpiredByExpiration(now)((key, value)) must_=== false
-    }
-    "is not expired if not set" in {
-      val now = HttpDate.unsafeFromEpochSecond(0) 
-      val key = CookieKey("foo", None, None)
-      val value = CookieValue(now, ResponseCookie(
-        "foo",
-        "bar",
-        expires = None
-      ))
-      CookieJar.isExpiredByExpiration(now)((key, value)) must_=== false
-    }
-  }
+  //   "show a cookie that is still valid" in {
+  //     val now = HttpDate.unsafeFromEpochSecond(0) 
+  //     val expired = HttpDate.unsafeFromEpochSecond(5000)
+  //     val key = CookieKey("foo", None, None)
+  //     val value = CookieValue(expired, ResponseCookie(
+  //       "foo",
+  //       "bar",
+  //       expires = Some(expired)
+  //     ))
+  //     CookieJar.isExpiredByExpiration(now)((key, value)) must_=== false
+  //   }
+  //   "is not expired if not set" in {
+  //     val now = HttpDate.unsafeFromEpochSecond(0) 
+  //     val key = CookieKey("foo", None, None)
+  //     val value = CookieValue(now, ResponseCookie(
+  //       "foo",
+  //       "bar",
+  //       expires = None
+  //     ))
+  //     CookieJar.isExpiredByExpiration(now)((key, value)) must_=== false
+  //   }
+  // }
 
-  "isExpiredByMaxAge" should {
-    "not be expired if the time is great enough" in {
-      val now = HttpDate.unsafeFromEpochSecond(5000) 
-      val key = CookieKey("foo", None, None)
-      val value = CookieValue(epoch, ResponseCookie(
-        "foo",
-        "bar",
-        maxAge = Some(5001L)
-      ))
-      CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== false
-    }
+  // "isExpiredByMaxAge" should {
+  //   "not be expired if the time is great enough" in {
+  //     val now = HttpDate.unsafeFromEpochSecond(5000) 
+  //     val key = CookieKey("foo", None, None)
+  //     val value = CookieValue(epoch, ResponseCookie(
+  //       "foo",
+  //       "bar",
+  //       maxAge = Some(5001L)
+  //     ))
+  //     CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== false
+  //   }
 
-    "be expired if the time is small enough" in {
-      val now = HttpDate.unsafeFromEpochSecond(5000) 
-      val key = CookieKey("foo", None, None)
-      val value = CookieValue(epoch, ResponseCookie(
-        "foo",
-        "bar",
-        maxAge = Some(4999L)
-      ))
-      CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== true
-    }
+  //   "be expired if the time is small enough" in {
+  //     val now = HttpDate.unsafeFromEpochSecond(5000) 
+  //     val key = CookieKey("foo", None, None)
+  //     val value = CookieValue(epoch, ResponseCookie(
+  //       "foo",
+  //       "bar",
+  //       maxAge = Some(4999L)
+  //     ))
+  //     CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== true
+  //   }
 
-    "be expired if it matches exactly" in {
-      val now = HttpDate.unsafeFromEpochSecond(5000) 
-      val key = CookieKey("foo", None, None)
-      val value = CookieValue(epoch, ResponseCookie(
-        "foo",
-        "bar",
-        maxAge = Some(500L)
-      ))
-      CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== true
-    }
+  //   "be expired if it matches exactly" in {
+  //     val now = HttpDate.unsafeFromEpochSecond(5000) 
+  //     val key = CookieKey("foo", None, None)
+  //     val value = CookieValue(epoch, ResponseCookie(
+  //       "foo",
+  //       "bar",
+  //       maxAge = Some(500L)
+  //     ))
+  //     CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== true
+  //   }
 
-    "not be expired if not set" in {
-      val now = HttpDate.unsafeFromEpochSecond(5000) 
-      val key = CookieKey("foo", None, None)
-      val value = CookieValue(epoch, ResponseCookie(
-        "foo",
-        "bar"
-      ))
-      CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== false
-    }
-  }
+  //   "not be expired if not set" in {
+  //     val now = HttpDate.unsafeFromEpochSecond(5000) 
+  //     val key = CookieKey("foo", None, None)
+  //     val value = CookieValue(epoch, ResponseCookie(
+  //       "foo",
+  //       "bar"
+  //     ))
+  //     CookieJar.isExpiredByMaxAge(now)((key, value)) must_=== false
+  //   }
+  // }
 
   "cookieAppliesToRequest" should {
     "apply if the given domain matches" in {
@@ -148,6 +148,7 @@ class CookieJarSpec extends Specification{
       )
       cookieAppliesToRequest(req,cookie) must_=== false
     }
+
 
     "not apply if the superdomain" in {
       val req = Request[IO](Method.GET, uri = Uri.uri("http://google.com"))
